f98d0e2d5dd7a3320d40b9ba5233234ff97e8181
Roman Timashev <roman@tmshv.ru>
2020-10-06 14:45:20 +0300

    next init with redirect

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    async redirects() {
+        return [
+           {
+               source: '/',
+               destination: 'https://app.latl.ng/map/FGF6683DZD8R4GE4',
+               permanent: true,
+           },
+        ]
+    }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const Page: NextPage = () => (
+    <div></div>
+)
+
+export default Page
