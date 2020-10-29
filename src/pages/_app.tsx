033babe9c09226c4941b851f930cccf822bbe29a
manhir <53478716+manhir@users.noreply.github.com>
2020-10-29 16:34:19 +0300

    upd ymetrika

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/style.css'
 
 import Head from 'next/head'
 import { AppType } from 'next/dist/next-server/lib/utils'
-import { YMetrika } from '../components/YMetrika'
 
 const App: AppType = props => {
     const { Component, pageProps } = props
@@ -12,7 +11,23 @@ const App: AppType = props => {
             <Head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, maximum-scale=1.0" />
-                <YMetrika />
+                <script type='text/javascript'
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                            (window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+                        
+                            ym(XXXXXXX, 'init', {
+                                clickmap:true,
+                                trackLinks:true,
+                                accurateTrackBounce:true,
+                                webvisor:true
+                            });
+                        `
+                    }}
+                />
+                {/* <noscript><div><img src='https://mc.yandex.ru/watch/XXXXXXX' style={{ position: 'absolute', left: -9999 }} alt='' /></div></noscript> */}
             </Head>
 
             <Component {...pageProps} />
