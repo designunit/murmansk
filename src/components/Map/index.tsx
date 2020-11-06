1d33066fa201b687d296f723453368f3df2538bd
manhir <53478716+manhir@users.noreply.github.com>
2020-11-06 15:41:39 +0300

    mobile map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -6,40 +6,38 @@ import s from './index.module.css'
 export const Map: React.FC = () => {
     return (
         <div className={s.container}>
+            <span>
             <Image
                 src='/static/map.png'
-                width={1152}
-                height={452}
-                layout='responsive'
+                // width={1152}
+                // height={452}
+                layout='fill'
             />
+            </span>
             <Link
                 href=''
             >
-            <a className={s.text}>
-                <Section style={{
-                    height: '100%',
+                <a className={s.text}>
+                    <Section style={{
+                        height: '100%',
 
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
 
-                    textTransform: 'uppercase',
-                    fontWeight: 'bold',
-                    fontSize: 50,
-                    lineHeight: '135%',
-                }}>
-                    <span>
-                        я много думаю о мурманске
-                </span>
-                    <span>
-                        у меня есть свое мнение
-                </span>
-                    <span>
-                        хочу чтобы его слышали
-                </span>
-                </Section>
-            </a>
+                        textTransform: 'uppercase',
+                        fontWeight: 'bold',
+                        fontSize: 50,
+                        lineHeight: '135%',
+                    }}>
+                        {['я много думаю о мурманске', 'у меня есть свое мнение', 'хочу чтобы его слышали'].map(x => (
+                            <span className={s.str}>
+                                {x}
+                            </span>
+                        ))}
+                    </Section>
+                </a>
             </Link>
         </div>
     )
