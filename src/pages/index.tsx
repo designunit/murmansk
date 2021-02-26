8d182cf56540e6005eabf8a1dc7a6112e15ba25e
manhir <53478716+manhir@users.noreply.github.com>
2021-02-26 20:10:23 +0300

    landing > index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,62 @@
+import Head from 'next/head'
 import { NextPage } from 'next'
+import { Layout } from '../components/Layout'
+import { GetStaticProps } from 'next'
+import { markdownToHtml } from '@/lib/markdownToHtml'
+import { Item } from '@/types'
+import { IMeta, Meta } from '@/components/Meta'
 
-const Page: NextPage = () => (
-    <div></div>
-)
+interface ILandingProps {
+    data: Item[]
+    meta: IMeta
+}
 
-export default Page
+const Landing: NextPage<ILandingProps> = ({ data, meta }) => {
+    return (
+        <>
+            <Head>
+                <title>Мойзалив.рф</title>
+                <Meta meta={meta} />
+            </Head>
+            <Layout data={data} />
+        </>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const res = await fetch(`https://unit.tmshv.com/mur-events`)
+    const data = await res.json()
+    const parsed: Item[] = await Promise.all(
+        data.map(async x => ({
+            ...x,
+            post: await markdownToHtml(x.post)
+        }))
+    )
+
+    const meta: IMeta = {
+		title: 'МОЙЗАЛИВ.РФ',
+		description: 'Дорогие Мурманчане, давайте чаще любоваться видами нашего города!',
+		image: 'https://мойзалив.рф/static/hero.png',
+		imageWidth: 911,
+		imageHeight: 1023,
+
+		url: 'https://мойзалив.рф/',
+		siteName: 'Дорогие Мурманчане, давайте чаще любоваться видами нашего города!',
+		locale: 'ru_RU',
+		type: 'website',
+		domain: 'https://мойзалив.рф',
+
+		twitterCard: 'summary_large_image',
+		twitterSite: '@',
+		twitterCreator: '@tmshv',
+	}
+
+    return {
+        props: {
+            data: parsed,
+            meta
+        }
+    }
+}
+
+export default Landing
