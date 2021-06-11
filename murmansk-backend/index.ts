12033df39ff11a6998823cabf79bdf91b6172f1c
Roman Timashev <roman@tmshv.ru>
2021-06-12 00:36:08 +0300

    format

--- murmansk-backend/index.ts
+++ murmansk-backend/index.ts
@@ -7,25 +7,27 @@ const app = express()
 app.use(express.json())
 
 app.get('/images', async (req, res) => {
-    const result = await prisma.image.findMany({
-        include: {
-            markers: true,
-        },
-    })
-    res.json(result)
+	const result = await prisma.image.findMany({
+		include: {
+			markers: true,
+		},
+	})
+	res.json(result)
 })
 
 app.post(`/images`, async (req, res) => {
-    const { src, width, height } = req.body as Prisma.ImageCreateInput
+	const { src, width, height } = req.body as Prisma.ImageCreateInput
 
-    const result = await prisma.image.create({
-        data: {
-            src,
-            width,
-            height,
-        }
-    })
-    res.json(result)
+	const result = await prisma.image.create({
+		data: {
+			src,
+			width,
+			height,
+		}
+	})
+	res.json(result)
+})
+
 app.post(`/vk`, async (req, res) => {
 	console.log('post vk', req.body)
 
@@ -66,109 +68,111 @@ app.post(`/vk`, async (req, res) => {
 })
 
 app.post(`/users`, async (req, res) => {
-    const { name, email } = req.body as Prisma.UserCreateInput
+	const { name, email, avatar } = req.body as Prisma.UserCreateInput
+	// const vkid = vk.id
 
-    try {
-        const result = await prisma.user.create({
-            data: {
-                name,
-                email,
-            },
-        })
-        res.json(result)
-    } catch (err) {
-        res.status(400).json({
-            error: err,
-        })
-    }
+	try {
+		const result = await prisma.user.create({
+			data: {
+				name,
+				email,
+				avatar,
+			},
+		})
+		res.json(result)
+	} catch (err) {
+		res.status(400).json({
+			error: err,
+		})
+	}
 })
 
 app.get('/users', async (req, res) => {
-    const users = await prisma.user.findMany()
-    res.json(users)
+	const users = await prisma.user.findMany()
+	res.json(users)
 })
 
 app.put(`/images/:image/like`, async (req, res) => {
-    const { image } = req.params
-    const imageId = parseInt(image)
-    const authorId = 1
-    const { like } = req.body as Prisma.LikeCreateInput
+	const { image } = req.params
+	const imageId = parseInt(image)
+	const authorId = 1
+	const { like } = req.body as Prisma.LikeCreateInput
 
-    try {
-        const result = await prisma.like.upsert({
-            where: {
-                imageId_authorId: {
-                    imageId,
-                    authorId
-                }
-            },
-            create: {
-                like,
-                imageId,
-                authorId,
-            },
-            update: {
-                like,
-            }
-        })
+	try {
+		const result = await prisma.like.upsert({
+			where: {
+				imageId_authorId: {
+					imageId,
+					authorId
+				}
+			},
+			create: {
+				like,
+				imageId,
+				authorId,
+			},
+			update: {
+				like,
+			}
+		})
 
-        const imageLikes = await prisma.like.findMany({
-            where: { imageId }
-        })
-        const likeCount = imageLikes.reduce((acc, value) => {
-            const x = value.like ? 1 : 0
-            return acc + x
-        }, 0)
-        const x = await prisma.image.update({
-            where: { id: imageId },
-            data: {
-                likeCount,
-            }
-        })
+		const imageLikes = await prisma.like.findMany({
+			where: { imageId }
+		})
+		const likeCount = imageLikes.reduce((acc, value) => {
+			const x = value.like ? 1 : 0
+			return acc + x
+		}, 0)
+		const x = await prisma.image.update({
+			where: { id: imageId },
+			data: {
+				likeCount,
+			}
+		})
 
-        res.json(result)
-    } catch (err) {
-        res.status(500).json({
-            error: err,
-        })
-    }
+		res.json(result)
+	} catch (err) {
+		res.status(500).json({
+			error: err,
+		})
+	}
 })
 
 app.post(`/images/:image/marker`, async (req, res) => {
-    const { image } = req.params
-    const imageId = parseInt(image)
-    const authorId = 1
-    const { content, x, y } = req.body as Prisma.MarkerCreateInput
-    const published = true
+	const { image } = req.params
+	const imageId = parseInt(image)
+	const authorId = 1
+	const { content, x, y } = req.body as Prisma.MarkerCreateInput
+	const published = true
 
-    try {
-        const result = await prisma.marker.create({
-            data: {
-                imageId,
-                authorId,
-                x,
-                y,
-                content,
-                published,
-            }
-        })
+	try {
+		const result = await prisma.marker.create({
+			data: {
+				imageId,
+				authorId,
+				x,
+				y,
+				content,
+				published,
+			}
+		})
 
-        res.json(result)
-    } catch (err) {
-        res.status(500).json({
-            error: err,
-        })
-    }
+		res.json(result)
+	} catch (err) {
+		res.status(500).json({
+			error: err,
+		})
+	}
 })
 
 app.get('/likes', async (req, res) => {
-    const authorId = 1
-    const result = await prisma.like.findMany({
-        where: { authorId }
-    })
-    res.json(result)
+	const authorId = 1
+	const result = await prisma.like.findMany({
+		where: { authorId }
+	})
+	res.json(result)
 })
 
-const server = app.listen(3000, () => {
-    console.log(`🚀 Server ready at: http://localhost:3000)`)
+const server = app.listen(8080, () => {
+	console.log(`🚀 Server ready at: http://localhost:8080)`)
 })
