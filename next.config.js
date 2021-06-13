f7143caec057e96b8e27b609aaff2fcf00846378
Roman Timashev <roman@tmshv.ru>
2021-06-13 21:21:27 +0300

    next config backend rewrites

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const backend = process.env.BACKEND_URL
+if (!backend) {
+	console.log('BACKEND_URL not set')
+	process.exit(1)
+}
 
 module.exports = {
     async rewrites() {
@@ -6,6 +11,10 @@ module.exports = {
                 source: `/${encodeURIComponent('опрос')}`,
                 destination: '/survey',
             },
+			{
+				source: '/api/v1/:path*',
+				destination: `${backend}/:path*`,
+			},
         ]
     }
 }
