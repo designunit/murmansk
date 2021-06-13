4347ea8cd504bf79af5d08859c61f32f67365ac6
Roman Timashev <roman@tmshv.ru>
2021-06-13 21:49:36 +0300

    rm Markers component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,16 @@ import { NextPage } from 'next'
 import { Layout } from '../components/Layout'
 import { GetStaticProps } from 'next'
 import { markdownToHtml } from '@/lib/markdownToHtml'
-import { Item, MarkerData } from '@/types'
+import { Item } from '@/types'
 import { IMeta, Meta } from '@/components/Meta'
 import React from 'react'
 import { Feed } from '@/components/Feed'
 import { Hero } from '@/components/Hero'
 import { Map } from '@/components/Map'
-import { Markers } from '@/components/Markers'
 
 interface ILandingProps {
     data: Item[]
     meta: IMeta
-    markers: MarkerData[]
 }
 
 const Landing: NextPage<ILandingProps> = ({ data, meta, markers }) => {    
@@ -27,7 +25,6 @@ const Landing: NextPage<ILandingProps> = ({ data, meta, markers }) => {
             <Layout data={data}>
                 <Hero />
                 <Map />
-                {false && <Markers markersData={markers} />}
                 {false && <Feed data={data} />}
             </Layout>
         </>
@@ -43,39 +40,6 @@ export const getStaticProps: GetStaticProps = async (context) => {
             post: await markdownToHtml(x.post)
         }))
     )
-
-    const markers = [
-        {
-            top: 10,
-            left: 40,
-            text: `Мой батя ебашит вообще адовые блюда. Ну такой вот примерно
-            рецепт усредненный, потому что вариаций масса. Берется суп, он
-            не греется, греть - это не про моего батю. Он берет это суп,
-            вываливает его на сковороду и начинает жарить. Добавляет в него
-            огромное количество лука, чеснока, перца черного и красного
-            МУКИ! для вязкости, томатная паста сверху. Все это жарится до
-            дыма. Потом снимается с огня и остужается на балконе. Потом батя
-            заносит и щедро полив майонезом начинает есть. При этом ест со
-            сковороды шкрябая по ней ложкой. Ест и приговаривает полушепотом
-            ух бля. При этом у него на лбу аж пот выступает. Любезно мне
-            иногда предлагает, но я отказываюсь. Надо ли говорить о том
-            какой дичайший пердеж потом? Вонища такая, что обои от стен
-            отклеиваются.`,
-            id: 'id1',
-        },
-        {
-            top: 33,
-            left: 76,
-            text: 'близкая точка для проверялки',
-            id: 'id2',
-        },
-        {
-            top: 34,
-            left: 70,
-            text: 'проверялка для близких точек',
-            id: 'id3',
-        },
-    ]
 
     const meta: IMeta = {
 		title: 'МОЙЗАЛИВ.РФ',
@@ -99,7 +63,6 @@ export const getStaticProps: GetStaticProps = async (context) => {
         props: {
             data: parsed,
             meta,
-            markers,
         }
     }
 }
