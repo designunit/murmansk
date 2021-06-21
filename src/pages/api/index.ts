505b27b29c04679e30f599824eff9f2dc329b9a0
manhir <53478716+manhir@users.noreply.github.com>
2021-06-21 17:04:25 +0300

    upd /api/index to handle projects forms

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -2,18 +2,44 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import axios, { AxiosResponse } from 'axios'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    res
-        .status(404)
-        .json({})
-    
-    return
-    await axios.post(
-        'https://script.google.com/macros/s/AKfycbx1nYKgCPmtaqG1zm9u7W6lNe3ByoFg6shXi9JMrq1DdBIJjtqGI-nh9qLi7obd_qaRkQ/exec',
-        req.body as string,
-    )
-        .then((axiosRes: AxiosResponse) => {
-            res
-                .status(axiosRes.status)
-                .json(axiosRes.data)
-        })
+    const getUrl = id => {
+        switch (id) {
+            case 'sklon-karla':
+                return (
+                    'https://script.google.com/macros/s/AKfycbyp8TexKZAqtUrsWjeiMbMwvtcT3GV5qdteQm2KBS_QYgclFRhMpxBetBeibezFjk_g7w/exec'
+                )
+            case 'abram-mys':
+                return (
+                    'https://script.google.com/macros/s/AKfycbw-1IBZIB7dNtxfRLRYEvHj6yJ3btYfZKl0PfKhidiX9bw7bKpR7ElNXGZznhGVVOw2Xw/exec'
+                )
+
+            default:
+                return (null)
+        }
+    }
+
+    const url = getUrl(JSON.parse(req.body).id)
+    if (url) {
+        res
+            .status(500)
+            .json({
+                result: 'error',
+                reason: 'invalid form id',
+            })
+
+        return
+    }
+
+    await fetch(
+        url,
+        {
+            method: 'post',
+            body: req.body as string
+        },
+    ).then(async value => {
+        console.log(value.status)
+        res
+            .status(value.status)
+            .json(await value.json())
+    })
 }
