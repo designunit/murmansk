680c5d5a2d60f32ea470220ff524f8671f86590a
manhir <53478716+manhir@users.noreply.github.com>
2021-12-09 20:25:57 +0300

    upd content (mayakovskogo)

--- src/components/ProjectsGrid/index.tsx
+++ src/components/ProjectsGrid/index.tsx
@@ -4,32 +4,45 @@ import { Emoji } from '../Emoji'
 import { Section } from '../Section'
 import s from './index.module.css'
 
+const Container = ({ href, children }) => href ? (
+    <Link href={href}>
+        <a className={s.a}>
+            {children}
+        </a>
+    </Link>
+) : (
+    <div className={s.a}>
+        {children}
+    </div>
+)
+
 export const ProjectsGrid: React.FC = () => {
     const data = [
         {
-            title: 'Запишись на строительство площадки Маяковского',
-            preview: null,
-            href: 'https://vk.com/moizaliv?w=wall-194862104_112',
-            description: (
-                <div
-                    style={{
-                        flex: '1 0 auto',
-                        border: 'solid 1px black',
-                        background: 'white',
-                        padding: '1rem',
-                        height: 200,
-                    }}
-                >
-                    {'Приглашаем ребят 11-16 лет придумать и построить площадку с видом на залив на ул. Маяковского '}
-                    <span>
-                        <Emoji name='⚒️' /> <Emoji name='👨‍👩‍👧‍👦' /> <Emoji name='🏘️' />
-                    </span>
-                </div>
-            )
+            title: 'Площадка Маяковского',
+            preview: '/static/mayakovskogo.jpg',
+            href: null, // 'https://vk.com/moizaliv?w=wall-194862104_112',
+            // preview: null,
+            // description: (
+            //     <div
+            //         style={{
+            //             flex: '1 0 auto',
+            //             border: 'solid 1px black',
+            //             background: 'white',
+            //             padding: '1rem',
+            //             height: 200,
+            //         }}
+            //     >
+            //         {'Приглашаем ребят 11-16 лет придумать и построить площадку с видом на залив на ул. Маяковского '}
+            //         <span>
+            //             <Emoji name='⚒️' /> <Emoji name='👨‍👩‍👧‍👦' /> <Emoji name='🏘️' />
+            //         </span>
+            //     </div>
+            // )
         },
         {
             title: 'Склон Либнекхта',
-            preview: '/static/projects/sklon-karla/7.jpg',
+            preview: '/static/projects/sklon-karla/preview.jpeg',
             href: '/karla-libnekhta',
         },
         {
@@ -73,34 +86,32 @@ export const ProjectsGrid: React.FC = () => {
                     className={s.grid}
                 >
                     {data.map((project, i) => (
-                        <Link href={project.href} key={i} >
-                            <a className={s.a}>
-                                <div style={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    overflow: 'clip',
-                                    whiteSpace: 'pre-wrap',
-                                    padding: '1rem',
-                                    height: '100%',
+                        <Container href={project.href} key={i} >
+                            <div style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                overflow: 'clip',
+                                whiteSpace: 'pre-wrap',
+                                padding: '1rem',
+                                height: '100%',
+                            }}>
+                                {project?.description}
+                                <img
+                                    src={project.preview}
+                                    style={{
+                                        display: 'block',
+                                        maxWidth: '100%',
+                                        height: !project?.description && 200,
+                                        objectFit: 'cover'
+                                    }}
+                                />
+                                <h3 style={{
+                                    marginBottom: 0,
                                 }}>
-                                    {project?.description}
-                                    <img
-                                        src={project.preview}
-                                        style={{
-                                            display: 'block',
-                                            maxWidth: '100%',
-                                            height: !project?.description && 200,
-                                            objectFit: 'cover'
-                                        }}
-                                    />
-                                    <h3 style={{
-                                        marginBottom: 0,
-                                    }}>
-                                        {project?.title}
-                                    </h3>
-                                </div>
-                            </a>
-                        </Link>
+                                    {project?.title}
+                                </h3>
+                            </div>
+                        </Container>
                     ))}
                 </div>
             </Section>
